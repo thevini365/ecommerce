f9d390d89b3ab9d02b3f33baf3f0c0011f5eedcf
Viniscius Ribeiro <viniscius.ribeiro@gmail.com>
2019-06-24 12:02:03 -0400

    we want code format consistency so we don't have to overload our brains with keeping up where things start or end, tab vs space, etc etc... spotless check for code format compliance and "spotlessApply" will format the code according to config, with this the repo should stay spotless!

--- server/src/main/java/rocks/tmov/EcommerceApp.java
+++ server/src/main/java/rocks/tmov/EcommerceApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApp {
-    public static void main(String[] args) {
-        SpringApplication.run(EcommerceApp.class);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(EcommerceApp.class);
+  }
 }
