7ac6ce4a23ca36c976cbea3fca3240e11b8e968d
Viniscius Ribeiro <viniscius.ribeiro@gmail.com>
2019-06-24 12:28:41 -0400

    opsy, forgot to pass on the args :scream: :see-no-evil:

--- server/src/main/java/rocks/tmov/EcommerceApp.java
+++ server/src/main/java/rocks/tmov/EcommerceApp.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommerceApp {
   public static void main(String[] args) {
-    SpringApplication.run(EcommerceApp.class);
+    SpringApplication.run(EcommerceApp.class, args);
   }
 }
